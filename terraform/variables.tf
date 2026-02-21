6029a58647c841f2899cd4de85fcce4a9eb806e5
AKASH8525 <akashak052004@gmail.com>
2026-02-21 17:24:29 +0000

    Fix root variables for CI/CD

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,4 +4,34 @@ variable "db_username" {}
 variable "db_password" {}
 variable "aws_region" {
   default = "ap-south-1"
+}variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "image_uri" {
+  description = "Docker image URI from ECR"
+  type        = string
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Database master username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database master password"
+  type        = string
+  sensitive   = true
 }
